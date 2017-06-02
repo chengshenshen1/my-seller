77783093a27e26b369df8fb81fff62264f03723d
chengshenshen1 <535653568@qq.com>
2017-06-02 18:54:53 +0800

    弹窗完成

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -16,13 +16,28 @@
             <span class="text">{{seller.supports[0].description}}</span>
           </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail()">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
   </div>
-  <div class="bulletin-wrapper">
+  <div class="bulletin-wrapper" @click="showDetail()">
+    <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+    <i class="icon-keyboard_arrow_right"></i>
+  </div>
+  <div class="background">
+    <img :src="seller.avatar" width="100%" height="100%">
+  </div>
+  <div class="detail" v-show="detailShow">
+    <div class="detail-wrapper clear-fix">
+      <div class="detail-main">
+        <h1 class="name">{{seller.name}}</h1>
+      </div>
+    </div>
+    <div class="detail-close">
+      <i class="icon-close"></i>
+    </div>
   </div>
 </div>
 </template>
@@ -33,6 +48,16 @@
         seller: {
             type: Object
         }
+    },
+    data () {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail () {
+          this.detailShow = true;
+      }
     },
     created () {
         this.classMap = ['decrease','discount','special','invoice','guarantee']
@@ -45,8 +70,9 @@
   @import "../../common/stylus/mixin.styl"
 
   .header
+    position relative
     color #fff
-    background #999
+    background rgba(7, 17, 27, 0.5)
     .content-wrapper
       padding 24px 12px 18px 24px
       font-size 0
@@ -95,6 +121,7 @@
             &special
               bg-image('special_1')
           .text
+            vertical-align top
             line-height 12px
             font-size 10px
 
@@ -114,4 +141,66 @@
           margin-left 2px
           line-height 24px
           font-size 10px
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 7px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        margin 0 4px
+        font-size 10px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      fliter blur(10px)
+    .detail
+      position fixed
+      top 0
+      left 0
+      z-index 100
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        width 100%
+        min-height 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            font-size 16px
+            text-align center
+            font-weight 700
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        margin -64px auto 0 auto
+        clear both
+        font-size 32px
 </style>
